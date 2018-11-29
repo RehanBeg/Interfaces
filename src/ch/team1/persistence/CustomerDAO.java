fee081cd09591ceffb6e607ee8e989aea227f2c6
Jan Minder <minderjan@gmail.com>
2018-11-29 19:39:36 +0100

    Added sample Interface Code

---
+++ src/ch/team1/persistence/CustomerDAO.java
@@ -0,0 +1,32 @@
+package ch.team1.persistence;
+
+public class CustomerDAO implements CustomerDAOIf {
+
+    private String firstname = "Nicht definiert";
+    private String lastname = "Nicht definiert";
+
+    @Override
+    public String getFirstName() {
+        return firstname;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastname;
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        this.firstname = firstName;
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.lastname = lastName;
+
+    }
+
+    public void lokaleMethode() {
+
+    }
+}
